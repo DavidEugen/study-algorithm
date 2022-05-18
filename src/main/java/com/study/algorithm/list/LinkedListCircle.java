d7b90945d9414344402042b0d06e58f01130c82b
DavidEugen <platonicojju@gmail.com>
2022-05-19 02:02:43 +0900

    Fix: 순환 연결 리스트 검증
    1. wrongAnswer() T: O(n2) S:O(1)
    노드를 돌며 노드가 중복되는 것이 있는지 검증한다.
    단순하게 생각하면 될것 같지만 => 순환에 빠진다.
    
    2. set을 이용하는 방법 //T: O(n)  S: O(n)
    공간을 하나 더 차지 한다.
    
    3. floydDetection
    트랙 문제 생각하면 된다.

--- src/main/java/com/study/algorithm/list/LinkedListCircle.java
+++ src/main/java/com/study/algorithm/list/LinkedListCircle.java
@@ -23,13 +23,30 @@ public class LinkedListCircle {
     //given
     public void print() {
         LinkedNode nodeToPrint = this.head;
-        while(nodeToPrint != null) {
+        while (nodeToPrint != null) {
             System.out.println(nodeToPrint.number);
             nodeToPrint = nodeToPrint.next;
         }
     }
 
-    boolean hasCircle1() {
+
+    boolean wrongAnswer() { // 노드를 순회하면서 현재 노드가 다음 노드 중에 중복으로 나타나는지 확인하는 방법 => 무한 루프 빠질 수 있다.
+        LinkedNode current = this.head;
+        while (current != null) {
+            LinkedNode temp = current.next;
+            while (temp != null) {
+                if (current == temp) {
+                    return true;
+                }
+                temp = temp.next;
+            }
+            current = current.next;
+        }
+
+        return false;
+    }
+
+    boolean hasCircle1() { //T: O(n)  S: O(n)
         Set<LinkedNode> nodeSet = new HashSet<>();
         LinkedNode current = this.head;
         while (current != null) {
@@ -45,26 +62,26 @@ public class LinkedListCircle {
         return false;
     }
 
-    boolean hasCircle2() {
+    boolean floydDetection() { //T: O(n)  S: O(1)
         LinkedNode slow = this.head;
         LinkedNode fast = this.head;
 
-        while(fast != null) {
-            if (fast.next == null || fast.next.next == null) {
+        while (fast != null) { // fast 는 slow 보다 앞서 감으로 fast만 보면 된다.
+            if (fast.next == null || fast.next.next == null) {  // fast 가 끝에 도달한다면 순환 생기지 않는다.
                 return false;
             }
 
+            //fast 2칸 전진
             fast = fast.next.next;
-            if (slow == fast) {
+            if (slow == fast) { // fast 가 slow를 만나면 => fast 가 slow를 따라 잡은 것이므로 있다.
                 return true;
             }
-
+            // slow 아니라면 한칸 전진
             slow = slow.next;
         }
 
         return false;
     }
-
 
 
 }
--- src/test/java/com/study/algorithm/list/LinkedListCircleTest.java
+++ src/test/java/com/study/algorithm/list/LinkedListCircleTest.java
@@ -7,6 +7,22 @@ import org.junit.jupiter.api.Test;
 
 class LinkedListCircleTest {
     @Test
+    @DisplayName("원형 LinkedList 검증 - 검증 실패")
+    void linkedListCircleFalseTest() {
+        LinkedListCircle list = new LinkedListCircle();
+        list.add(new LinkedNode(1));
+        list.add(new LinkedNode(2));
+//        LinkedNode node3 = new LinkedNode(3);
+//        list.add(node3);
+        list.add(new LinkedNode(4));
+        list.add(new LinkedNode(5));
+//        list.add(node3);
+
+//        list.print();
+        System.out.println(list.wrongAnswer());
+    }
+
+    @Test
     @DisplayName("원형 LinkedList 검증")
     void linkedListCircleTest() {
         LinkedListCircle list = new LinkedListCircle();
@@ -20,6 +36,22 @@ class LinkedListCircleTest {
 
 //        list.print();
         System.out.println(list.hasCircle1());
+    }
+
+    @Test
+    @DisplayName("원형 LinkedList 검증 - 플로이드의 토끼와 거북이 알고리즘")
+    void floydDetectionTest() {
+        LinkedListCircle list = new LinkedListCircle();
+//        list.add(new LinkedNode(1));
+//        list.add(new LinkedNode(2));
+//        LinkedNode node3 = new LinkedNode(3);
+//        list.add(node3);
+//        list.add(new LinkedNode(4));
+//        list.add(new LinkedNode(5));
+//        list.add(node3);
+
+//        list.print();
+        System.out.println(list.floydDetection());
     }
 
 
